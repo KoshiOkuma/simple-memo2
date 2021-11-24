34d709651dc76c798592dd3e0afdb7ed550ee99a
KoshiOkuma <koshi.glorydays1128@gmail.com>
2021-11-24 12:07:17 +0900

    update, destroyメソッド追加,config/app.php修正

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -2,13 +2,19 @@
 
 @section('content')
 <div class="card">
-    <div class="card-header">メモ変数</div>
-    <form class="card-body" action="{{ route('store') }}" method="post">
+    <div class="card-header">
+        メモ編集
+        <form action="{{ route('destroy')}}" method="post">
+            @csrf
+            <input type="hidden" name="memo_id" value="{{ $edit_memo['id'] }}">
+            <button type="submit">削除</button>
+        </form>
+    </div>
+    <form class="card-body" action="{{ route('update') }}" method="post">
         @csrf
+        <input type="hidden" name="memo_id" value="{{ $edit_memo['id'] }}">
         <div class="form-group">
-            <textarea class="form-control" name="content" rows="3" placeholder="ここに入力">
-                {{ $edit_memo['content'] }}
-            </textarea>
+            <textarea class="form-control" name="content" rows="3" placeholder="ここに入力">{{ $edit_memo['content'] }}</textarea>
           </div>
           <button type="submit" class="btn btn-primary">更新</button>
         </form>
